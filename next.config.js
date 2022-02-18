a6c1e48b1beb8236c78f59237d79326efb6f6b2c
Chris Vickery <chris.vickery@dutchie.com>
2022-02-18 00:46:55 -0800

    base paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/proofofcombat/",
+  assetPrefix: "/proofofcombat/",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
