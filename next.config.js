37faf363f019ff6971ab27a3112438f229514c6c
Chris Vickery <chris.vickery@dutchie.com>
2022-02-18 00:50:43 -0800

    what an outrageous error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/proofofcombat/",
+  basePath: "/proofofcombat",
   assetPrefix: "/proofofcombat/",
   reactStrictMode: true,
 };
