63fcd51797a7e5cf60ccbafeacb6ef21bd9a99a1
Chris Vickery <chris.vickery@dutchie.com>
2024-02-26 20:35:34 -0800

    no ssr the logout button

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useApolloClient } from "@apollo/client";
 import { useRouter } from "next/router";
 
+import NoSsr from "@mui/material/NoSsr";
 import Button from "@mui/material/Button";
 
 import { useToken } from "src/token";
@@ -22,14 +23,16 @@ export function LogoutButton(): JSX.Element | null {
   }
 
   return (
-    <Button
-      aria-label="Logout and return to login screen"
-      color="error"
-      variant="contained"
-      disableElevation
-      onClick={handleLogout}
-    >
-      Logout
-    </Button>
+    <NoSsr>
+      <Button
+        aria-label="Logout and return to login screen"
+        color="error"
+        variant="contained"
+        disableElevation
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+    </NoSsr>
   );
 }
